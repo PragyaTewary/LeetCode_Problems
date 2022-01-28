f89a95d531ee544b02619c35d1a62fdfc48c4a2b
Pragya Tewary <69004214+PragyaTewary@users.noreply.github.com>
2022-01-28 20:41:31 +0530

    Time: 301 ms (58.14%), Space: 82.8 MB (72.09%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.cpp
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.cpp
@@ -2,38 +2,17 @@ class Solution {
 public:
     int reductionOperations(vector<int>& nums) {
         int n = nums.size();
-        int mv = *min_element(nums.begin(), nums.end());
         int ans = 0;
-        sort(nums.rbegin(), nums.rend());
+        sort(nums.begin(), nums.end());
         int prev = nums[0];
         int c = 0;
-        for(int i=0; i<n; i++)
+        for(int i=n-1; i>=1; i--)
         {
-            if(nums[i] == mv)
+            if(nums[i] != nums[i-1])
             {
-                ans += c;
-                return ans;
-            }
-            else if(nums[i] == prev)
-            {
-               c++;
-            }
-            else
-            {
-                ans += c;
-                prev = nums[i];
-                c++;
+                ans += n-i;
             }
         }
-        // unordered_map<int, int> mp;
-        // for(int i=0; i<n; i++)
-        // {
-        //     mp[nums[i]]++;
-        //     mv = min(mv, nums[i]);
-        // }
-        // if(mp[mv] == n)
-        //     return 0;
         return ans;
-        // 1 1 2 4 5 5 5 6 6
     }
 };
